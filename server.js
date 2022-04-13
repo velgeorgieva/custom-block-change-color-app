d38db692f91b35554ced8c41a5b3b156aa0ef623
velinsi <velin401@gmail.com>
2022-04-13 16:24:48 +0300

    Add cors policy in server js

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+
+const cors = require('cors');
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -13,4 +22,10 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.post("/", (req, res) => {
+    // perform operation and return response.
+    res.status(200).json({ message: "It worked!" });
+});
+
 app.listen(port);
