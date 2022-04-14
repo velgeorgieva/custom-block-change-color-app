bae26110b6f0791d5108ef5ea656d84a241ffecb
velinsi <velin401@gmail.com>
2022-04-14 12:14:30 +0300

    Add console log in server js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json())
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
