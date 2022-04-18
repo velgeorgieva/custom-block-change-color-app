9e2c4bddb1c149cb8b41dd60825d2f8727e31349
velinsi <velin401@gmail.com>
2022-04-18 23:01:11 +0300

    Change postMessage signature

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function toggleTheme() {
   const body = document.querySelector('.App-header');
   body.classList.toggle('dark');  // toggle dark class
-  window.parent.postMessage({ "data": {"msg": "sportal365_custom_event", "body": '<h1>Succeeded!</h1>'}}, '*')
+  window.parent.postMessage({ "data": {"msg": "sportal365_custom_event", "body": document}}, '*')
 }
 
 function App() {
