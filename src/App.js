187fd8f98354c99a9f63089f846338a6d36632c6
velinsi <velin401@gmail.com>
2022-04-19 18:47:33 +0300

    Change postMessage signature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function toggleTheme() {
   const body = document.querySelector('.App-header');
   body.classList.toggle('dark');  // toggle dark class
   const jsonResponse = {"name":"John", "age":30, "car":null};
+  console.log(JSON.stringify(jsonResponse));
   window.parent.postMessage({ "data": {"msg": "sportal365_custom_event", "payload": JSON.stringify(jsonResponse)}, '*')
 }
 //Callback function
